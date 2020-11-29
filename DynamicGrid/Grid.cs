9ee5fe9d94e445fcefa736d3b72d72a478c95b30
Tomasz Rewak <tomaszrewak@gmail.com>
2020-11-29 19:18:10 +0100

    Cleanup

--- DynamicGrid/Grid.cs
+++ DynamicGrid/Grid.cs
@@ -1,12 +1,9 @@
 ﻿using DynamicGrid.Buffers;
 using DynamicGrid.Threading;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DynamicGrid
